3dc189090897a4deeebcff9ebd80ece1ac280b35
dimdimich123 <frostm333@users.noreply.github.com>
2022-09-17 14:21:49 +0700

    Create script PlanetaryStarshipFactory.cs

---
+++ Assets/Scripts/PlanetaryStarshipFactory.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+//[RequireComponent(typeof(Planet))]
+public sealed class PlanetaryStarshipFactory : MonoBehaviour
+{
+    private const float _timeBetweenGeneration = 1f;
+    private const int _generateCount = 5;
+    public int ShipCount { get; private set; } = 0;
+
+    public event Action<int> OnShipCreate;
+
+    void Start()
+    {
+        StartCoroutine(GenerateShips());
+    }
+
+    public void Init(int shipCount)
+    {
+        ShipCount = shipCount;
+    }
+
+    private IEnumerator GenerateShips()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(_timeBetweenGeneration);
+            ShipCount += _generateCount;
+            OnShipCreate?.Invoke(ShipCount);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
