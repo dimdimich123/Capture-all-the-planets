b5f8d136ebb30b9349ef146bee2a45578bfa0fe4
dimdimich123 <frostm333@gmail.com>
2022-10-05 17:59:53 +0700

    Easy enemy AI

--- Assets/Scripts/AI/EasyEnemy.cs
+++ Assets/Scripts/AI/EasyEnemy.cs
@@ -1,18 +1,55 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
-public class EasyEnemy : MonoBehaviour
+public class EasyEnemy : EnemyAI
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override IEnumerator TryAttack()
     {
-        
+        while (true)
+        {
+            _attackingPlanet = null;
+            _attackedPlanet = null;
+            _attackingPlanet = FindAttackingPlanet();
+            _attackedPlanet = FindAttackedPlanet();
+            if (_attackingPlanet && _attackedPlanet)
+            {
+                AttackPlanet();
+            }
+            yield return new WaitForSeconds(1f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void AttackPlanet()
     {
-        
+        _attackingPlanet.AttackPlanet(_attackedPlanet.transform);
+    }
+
+    protected override PlanetController FindAttackedPlanet()
+    {
+        List<PlanetController> _canAttackPlanet = new List<PlanetController>();
+        foreach (PlanetController planet in _planets)
+        {
+            if (planet.State != PlanetState.Enemy && planet.ShipCount < _attackingPlanet.ShipCount / 2)
+            {
+                _canAttackPlanet.Add(planet);
+            }
+        }
+        return _canAttackPlanet[Random.Range(0, _canAttackPlanet.Count + 1)];
+    }
+
+    protected override PlanetController FindAttackingPlanet()
+    {
+        PlanetController attackingPlanet = null;
+        int maxShipCount = 0;
+        foreach (PlanetController planet in _planets)
+        {
+            if (planet.State == PlanetState.Enemy && planet.ShipCount > maxShipCount)
+            {
+                attackingPlanet = planet;
+                maxShipCount = attackingPlanet.ShipCount;
+            }
+        }
+        return attackingPlanet;
     }
 }
